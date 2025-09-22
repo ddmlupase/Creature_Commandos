89ea5e95a0d3b49205eb03aed17ab93bc5b48636
Daniel David <146162731+ddmlupase@users.noreply.github.com>
2025-09-22 21:58:28 +0800

    feat: add navigation and multiple pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
+import Nav from './nav';
+import Home from './pages/home';
+import About from './pages/about';
+import Login from './pages/login';
+import Error from './pages/error';
+import Characters from './pages/characters';
+
+const NavBarLayout = () => (
+  <>
+    <Nav />
+    <Outlet />
+  </>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<NavBarLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/characters" element={<Characters />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/error" element={<Error />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
